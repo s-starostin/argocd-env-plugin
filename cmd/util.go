40d6beb1eb9bb6ac6d05191f29276a1f47c8c27a
ЦЦТ-Старостин Сергей Юрьевич <sergey.starostin@tatar.ru>
2021-08-17 21:44:39 +0300

    update

--- cmd/util.go
+++ cmd/util.go
@@ -48,9 +48,7 @@ func listYamlFiles(root string) ([]string, error) {
 	var files []string
 
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-	    if info.IsDir() {
-            return filepath.SkipDir
-        }
+	    fmt.Errorf("xxx %s", filepath)
 		if filepath.Ext(path) == ".yaml" || filepath.Ext(path) == ".yml" {
 			files = append(files, path)
 		}
